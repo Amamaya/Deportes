735e358de5217a581d7d154b0f32bb9e975be229
Alumno Administración <aadmon@utags.edu.mx>
2022-08-09 15:43:32 -0500

    Version 2

---
+++ js/jf.js
@@ -0,0 +1,16 @@
+function bienvenida(){
+
+    var hola1 = Text($("#hola1").val());
+
+    var bienve = (hola1);
+    var bien = document.getElementById("bienvenid");
+    const result1 = document.getElementById("mensaje1");
+
+    result1.innerText = bienve + "Que tengas un buen día";
+}
+
+function bienvenida(){
+    var hola1 = Text($("#hola1").val());
+    const hola1 = document.getElementById("mensaje1");
+    hola1.innerText = hola1;
+}
